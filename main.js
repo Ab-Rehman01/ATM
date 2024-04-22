741f8afdbe80d26f81fc3ce80066819393f28e8f
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2024-04-22 07:02:51 +0500

    First ATM

---
+++ main.js
@@ -0,0 +1,68 @@
+#! /usr/bin/env node
+// shabang
+import inquirer from "inquirer";
+let yourBalance = 50000;
+let myPin = 3948;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pincode",
+        message: "enter your pin",
+        type: "number",
+    }
+]);
+//console.log(pinAnswer)
+if (pinAnswer.pincode === myPin) {
+    console.log("correct pin code!!!");
+    let operationAnswer = await inquirer.prompt({
+        name: "operation",
+        message: "please select option",
+        type: "list",
+        choices: ["withdraw", "check balance", "transfer to an other account"],
+    });
+    console.log(operationAnswer);
+    if (operationAnswer.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: " enter your amount",
+                type: "number",
+            }
+        ]);
+        if (amountAns.amount <= yourBalance) {
+            yourBalance -= amountAns.amount;
+            console.log("your remaing balance is:" + yourBalance);
+        }
+        else
+            (console.log("your balance is insufficient"));
+    }
+    else 
+    //check balance
+    if (operationAnswer.operation === "check balance") {
+        console.log("your balance is:" + yourBalance);
+    }
+    else 
+    // Transfer to other account
+    if (operationAnswer.operation === "transfer to an other account") {
+        let amountTransfer = await inquirer.prompt([
+            {
+                name: "transfer",
+                message: " enter your 5 digit account number",
+                type: "number",
+            },
+            {
+                name: "transferAmount",
+                message: "enter your amout",
+                type: "number",
+            }
+        ]);
+        if (amountTransfer.transferAmount <= yourBalance) {
+            yourBalance -= amountTransfer.transferAmount;
+            console.log("your remaing balance is:" + yourBalance);
+        }
+        else
+            (console.log("your balance is insufficient"));
+    }
+}
+else {
+    console.log("incorrect pin number");
+}
